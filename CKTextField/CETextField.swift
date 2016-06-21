85d6d4f8c07cde8dc69bfb41c82bc4ee75a4faa4
congHu <850476264@qq.com>
2016-06-21 16:21:34 +0800

    New CE Namespace

---
+++ CKTextField/CETextField.swift
@@ -0,0 +1,30 @@
+//
+//  CETextField.swift
+//  
+//
+//  Created by David on 16/6/21.
+//
+//
+
+import UIKit
+
+class CETextField: UITextField {
+
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
